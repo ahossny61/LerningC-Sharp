77150ac6894f2a8ca4a88d54d6525d99484ce09c
ahossny61 <ahossny61@gmail.com>
2023-07-01 13:14:45 +0300

    Index

---
+++ Learning/IP.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    // Index Example
+    public class IP
+    {
+        private int[] segments = new int[4];
+
+        public int this[int index]
+        {
+            get { return segments[index]; }
+            set { segments[index] = value;}
+        }
+        public IP(int segment1 , int segment2, int segment3, int segment4)
+        {
+            segments[0] = segment1;
+            segments[1] = segment2;
+            segments[2] = segment3;
+            segments[3] = segment4;
+        }
+        public IP(string ip)
+        {
+            var result = ip.Split('.').Select(e=>int.Parse(e));
+            segments = result.ToArray();
+        }
+
+        public string GetIP =>string.Join(".", segments);
+    }
+}
--- Learning/Program.cs
+++ Learning/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Learning
 {
@@ -12,6 +13,13 @@ namespace Learning
 
             Employee emp = Employee.Create(1,"Ahmed","Hosny");
             Console.WriteLine( emp.DisplayName());
+
+            var ip = new IP(192, 162, 0, 0);
+            ip[3] = 15;
+            Console.WriteLine(ip.GetIP);
+
+            var ip2 = new IP("192.168.1.0");
+            Console.WriteLine(ip2.GetIP);
 
         }
     }
