1ede0f43ba964e521b3b1039ff901a55ddb4206d
ahossny61 <ahossny61@gmail.com>
2023-07-01 13:47:05 +0300

    Delegate Example

---
+++ Delegate/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using Delegate;
+
+var employees = new Employee[]
+{
+    new Employee
+    {
+        Id = 1,
+        TotalSalary= 5869,
+        Name = "Ahmed",
+        Gender = "male"
+    },
+     new Employee
+    {
+        Id = 1,
+        TotalSalary= 6899,
+        Name = "Aml",
+        Gender = "female"
+    },
+      new Employee
+    {
+        Id = 1,
+        TotalSalary= 3325,
+        Name = "Asd",
+        Gender = "male"
+    },
+       new Employee
+    {
+        Id = 1,
+        TotalSalary= 10000,
+        Name = "Aid",
+        Gender = "male"
+    },
+};
+
+Report report = new Report();
+report.SalaryWithAndPlus6000(employees);
+report.DelegateExample(employees, "Employees that greater than 2000",(Employee emp)=>emp.TotalSalary > 2000);
---
+++ Delegate/Report.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class Report
+    {
+        public delegate bool Condition(Employee emp) ; 
+        public void SalaryWithAndPlus6000(Employee[] employees)
+        {
+            Console.WriteLine("The employees that more than 6000 salary :");
+            foreach(Employee emp in employees)
+            {
+                if(emp.TotalSalary >= 6000)
+                {
+                    Console.WriteLine($"Name : {emp.Name} , it's salary = {emp.TotalSalary}");
+                }
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void SalaryBetween3000And5999(Employee[] employees)
+        {
+            Console.WriteLine("The employees that Between 3000 And 5999 salary :");
+            foreach (Employee emp in employees)
+            {
+                if (emp.TotalSalary >= 3000 && emp.TotalSalary < 6000)
+                {
+                    Console.WriteLine($"Name : {emp.Name} , it's salary = {emp.TotalSalary}");
+                }
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void DelegateExample(Employee[] employees,string title , Condition condition)
+        {
+            Console.WriteLine(title);
+            foreach (Employee emp in employees)
+            {
+                if (condition(emp))
+                {
+                    Console.WriteLine($"Name : {emp.Name} , it's salary = {emp.TotalSalary}");
+                }
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
