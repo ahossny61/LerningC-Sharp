4b3eb7ed88e81bff7b7ceac126816ede1f7a6aad
ahossny61 <ahossny61@gmail.com>
2023-08-20 10:51:09 +0300

    ..

---
+++ Real Examples/BankAccount.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_Examples
+{
+    public class BankAccount
+    {
+        private static int s_accountNumberSeed = 1234567890;
+        private List<Transaction> transactions;
+        public string Number { get;  }
+        public string Owner { get; set; }
+        public decimal Balance { 
+            get 
+            {
+                decimal balance = 0;
+                foreach (var transaction in transactions)
+                {
+                    balance += transaction.Amount;
+                }
+                return balance;
+            }
+           
+        }
+
+        public BankAccount(string name , decimal initialBalance)
+        {
+            Owner= name;
+            
+            Number = s_accountNumberSeed.ToString();
+            s_accountNumberSeed++;
+            transactions = new List<Transaction>();
+            MakeDeposit(initialBalance, DateTime.Now,"Initial balance");
+        }
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0){
+
+                throw new ArgumentOutOfRangeException (nameof(amount), "Amount of deposit must be positive");
+            }
+
+            transactions.Add(new Transaction(amount, date,note));
+            
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if(Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            transactions.Add(new Transaction(-amount, date, note));
+        }
+
+        //public string GetAccountHistory()
+        //{
+        //    var 
+        //}
+
+    }
+}
---
+++ Real Examples/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+using Real_Examples;
+using System.Security.Principal;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+StringBuilder result = new StringBuilder();
+string input = Console.ReadLine();
+bool isNumberStarted = false;
+
+foreach (char c in input)
+{
+    if (char.IsDigit(c))
+    {
+        result.Append(c);
+        isNumberStarted = true;
+    }
+    else if (c == '-' && !isNumberStarted)
+    {
+        result.Append(c);
+        isNumberStarted = true;
+    }
+    else
+    {
+        isNumberStarted = false;
+    }
+}
+
+Console.WriteLine(result);
+//try
+//{
+//    BankAccount bankAccount = new BankAccount("Ahmed", 1500);
+
+//    Console.WriteLine($"Bank Account Number is {bankAccount.Number}");
+
+//    bankAccount.MakeDeposit(1500, DateTime.Now, "");
+//    Console.WriteLine($"your balance is {bankAccount.Balance}");
+//    bankAccount.MakeWithdrawal(2500, DateTime.Now, "");
+//    Console.WriteLine($"your balance is {bankAccount.Balance}");
+//    bankAccount.MakeWithdrawal(2500, DateTime.Now, "");
+//    Console.WriteLine($"your balance is {bankAccount.Balance}");
+
+
+//}
+//catch (InvalidOperationException e)
+//{
+//    Console.WriteLine("Exception caught trying to overdraw");
+//    Console.WriteLine(e.ToString());
+//}
+
+
+
+
